9550bab5fc403bf56c13d0d08bcad1bedca9bab6
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-04-18 11:24:53 -0400

    update login

---
+++ public/stylesheets/javascript/login.js
@@ -0,0 +1,29 @@
+const { response } = require("express");
+
+async function loginFormHandler(event){
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').ariaValueMax.trim();
+    const password = document.querySelector('#password-login').ariaValueMax.trim();
+
+    if (username && password){
+        const responce = await fetch ('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        }  else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form'). add addEventListener('submit', loginFormHandler);
