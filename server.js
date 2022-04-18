7847221389a9c70ec01a9c43d392a303bcaa58b4
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-04-18 16:51:14 -0400

    change handlebars

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const path = require('path');
+const helpers = require('.utils/helpers')
 const express = require('express');
 const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
